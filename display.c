9c790a657e5df35cf57976e4014df337f39feeae
Kencho5 <giokenchadze@gmail.com>
2024-05-11 16:05:56 +0400

    Updated: made scene 3d and added a cube

--- display.c
+++ display.c
@@ -1,7 +1,34 @@
 #include </opt/homebrew/include/GL/glut.h>
 
 void display() {
-  glClear(GL_COLOR_BUFFER_BIT);
-  // rendering code goes here
-  glFlush();
+  // Clear the color and depth buffers
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  // Set up perspective projection
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluPerspective(45.0f, 800.0f / 600.0f, 0.1f, 100.0f);
+
+  // Set up the view transformation (position and orientation of the camera)
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+  gluLookAt(0, 0, 5, 0, 0, 0, 0, 1, 0);
+
+  // rendering code for 3D objects goes here
+  glBegin(GL_QUADS);
+  // Front face
+  glColor3f(1.0f, 0.0f, 0.0f); // Red
+  glVertex3f(-1.0f, -1.0f, 1.0f);
+  glVertex3f(1.0f, -1.0f, 1.0f);
+  glVertex3f(1.0f, 1.0f, 1.0f);
+  glVertex3f(-1.0f, 1.0f, 1.0f);
+  // Back face
+  glColor3f(0.0f, 1.0f, 0.0f); // Green
+  glVertex3f(-1.0f, -1.0f, -1.0f);
+  glVertex3f(-1.0f, 1.0f, -1.0f);
+  glVertex3f(1.0f, 1.0f, -1.0f);
+  glVertex3f(1.0f, -1.0f, -1.0f);
+
+  glEnd();
+  glutSwapBuffers();
 }
--- main.c
+++ main.c
@@ -6,12 +6,14 @@ void keyPressed(unsigned char key, int x, int y);
 
 int main(int argc, char **argv) {
   glutInit(&argc, argv);
-
+  glutInitWindowSize(800, 600);
   glutCreateWindow("Test");
+
+  // Enable depth testing
+  glEnable(GL_DEPTH_TEST);
+
   glutDisplayFunc(display);
-
   glutKeyboardFunc(keyPressed);
-
   glutMainLoop();
   return 0;
 }
