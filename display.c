289af3beffe8f956fb530703296e7940d4f20dd3
Kencho5 <giokenchadze@gmail.com>
2024-05-11 16:12:23 +0400

    Added: more cube faces

--- display.c
+++ display.c
@@ -12,10 +12,11 @@ void display() {
   // Set up the view transformation (position and orientation of the camera)
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
-  gluLookAt(0, 0, 5, 0, 0, 0, 0, 1, 0);
+  gluLookAt(5, 5, 5, 0, 0, 0, 0, 1, 0);
 
   // rendering code for 3D objects goes here
   glBegin(GL_QUADS);
+
   // Front face
   glColor3f(1.0f, 0.0f, 0.0f); // Red
   glVertex3f(-1.0f, -1.0f, 1.0f);
@@ -25,9 +26,33 @@ void display() {
   // Back face
   glColor3f(0.0f, 1.0f, 0.0f); // Green
   glVertex3f(-1.0f, -1.0f, -1.0f);
+  glVertex3f(1.0f, -1.0f, -1.0f);
+  glVertex3f(1.0f, 1.0f, -1.0f);
+  glVertex3f(-1.0f, 1.0f, -1.0f);
+  // Top face
+  glColor3f(0.0f, 0.0f, 1.0f); // Blue
   glVertex3f(-1.0f, 1.0f, -1.0f);
   glVertex3f(1.0f, 1.0f, -1.0f);
+  glVertex3f(1.0f, 1.0f, 1.0f);
+  glVertex3f(-1.0f, 1.0f, 1.0f);
+  // Bottom face
+  glColor3f(1.0f, 1.0f, 0.0f); // Yellow
+  glVertex3f(-1.0f, -1.0f, -1.0f);
+  glVertex3f(-1.0f, -1.0f, 1.0f);
+  glVertex3f(1.0f, -1.0f, 1.0f);
   glVertex3f(1.0f, -1.0f, -1.0f);
+  // Right face
+  glColor3f(1.0f, 0.0f, 1.0f); // Magenta
+  glVertex3f(1.0f, -1.0f, -1.0f);
+  glVertex3f(1.0f, 1.0f, -1.0f);
+  glVertex3f(1.0f, 1.0f, 1.0f);
+  glVertex3f(1.0f, -1.0f, 1.0f);
+  // Left face
+  glColor3f(0.0f, 1.0f, 1.0f); // Cyan
+  glVertex3f(-1.0f, -1.0f, -1.0f);
+  glVertex3f(-1.0f, -1.0f, 1.0f);
+  glVertex3f(-1.0f, 1.0f, 1.0f);
+  glVertex3f(-1.0f, 1.0f, -1.0f);
 
   glEnd();
   glutSwapBuffers();
